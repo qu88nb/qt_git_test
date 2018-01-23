077c2b0801c08cfa47c07abf0736b25b44883e8d
qu88nb <qu88nb@163.com>
2018-01-23 18:51:37 +0800

    Ver 1.1
    This is the second version.

--- dialog.cpp
+++ dialog.cpp
@@ -1,3 +1,4 @@
+#include <QMessageBox>
 #include "dialog.h"
 #include "ui_dialog.h"
 
@@ -6,9 +7,30 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    m_bFlag = false;
+    ui->pushButton->setText("show");
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(showText()));
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::showText()
+{
+    if (m_bFlag)
+    {
+        ui->pushButton->setText("show");
+        ui->textEdit->clear();
+        QMessageBox::information(this, "Info", "clear text info ok!");
+    }
+    else
+    {
+        ui->pushButton->setText("clear");
+        ui->textEdit->setText("hi qu88nb, welcome to qt github!");
+    }
+
+    m_bFlag = !m_bFlag;
+    return;
 }
